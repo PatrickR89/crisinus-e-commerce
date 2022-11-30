e834c6f7d6e7fa8e767d2177e53ae601b52fc577
PatrickR89 <patrickr.89sc@gmail.com>
2022-11-30 20:23:25 +0100

    Crisinus Client - [Add] slide-in news animation

--- src/components/public/news/News.js
+++ src/components/public/news/News.js
@@ -8,27 +8,69 @@ const News = ({ newsPage, home }) => {
   const { fetchSingleNews, news, single_news } = useItemsContext();
 
   const [tempIndex, setTempIndex] = useState(0);
-  var newsArray = [];
+  const [listItems, setListItems] = useState([]);
+  const [newsArray, setNewsArray] = useState([]);
+
+  useEffect(() => {
+    // news.forEach((singleNews) => {
+    //   setNewsArray([...newsArray, singleNews.id]);
+    // });
+    // console.log(newsArray);
+    let documentLIs = document.querySelectorAll(".li-item");
+    setListItems(documentLIs);
+  }, []);
 
   useEffect(() => {
     if (home && news.length) {
-      news.forEach((singleNews) => {
-        newsArray.push(singleNews.id);
-      });
-      console.log(newsArray);
+      let documentLIs = document.querySelectorAll(".li-item");
+      setListItems(documentLIs);
       const indexTimeout = setTimeout(() => {
         if (tempIndex > 0) {
           setTempIndex(tempIndex - 1);
         } else {
           setTempIndex(news.length - 1);
         }
-      }, 10000);
-      fetchSingleNews(news[tempIndex].id);
+      }, 8000);
+      // fetchSingleNews(news[tempIndex].id);
+      setCurrentNews(tempIndex);
       return () => {
         clearTimeout(indexTimeout);
       };
     }
   }, [tempIndex, news]);
+
+  function setCurrentNews(index) {
+    var nextNewsIndex = index + 1;
+    if (nextNewsIndex > news.length - 1) {
+      nextNewsIndex = 0;
+    }
+
+    var previousNewsIndex = index - 1;
+    if (previousNewsIndex < 0) {
+      previousNewsIndex = news.length - 1;
+    }
+    let previousNews = document.getElementById(`${news[nextNewsIndex].id}`);
+    let currentNews = document.getElementById(`${news[index].id}`);
+    let nextNews = document.getElementById(`${news[previousNewsIndex].id}`);
+
+    previousNews.classList.remove("active");
+    previousNews.classList.remove("right");
+    previousNews.classList.add("left");
+
+    nextNews.classList.add("right");
+    nextNews.classList.remove("left");
+    nextNews.classList.remove("active");
+
+    currentNews.classList.remove("right");
+    currentNews.classList.remove("left");
+    currentNews.classList.add("active");
+
+    setTimeout(() => {
+      previousNews.classList.add("hide");
+      nextNews.classList.remove("hide");
+    }, 900);
+    console.log(currentNews);
+  }
 
   return (
     <main>
@@ -36,7 +78,11 @@ const News = ({ newsPage, home }) => {
         <ul className="n-page single single-home">
           {news.map((singleNews) => {
             return (
-              <li key={singleNews.id} className="li-item right">
+              <li
+                key={singleNews.id}
+                id={singleNews.id}
+                className="li-item right"
+              >
                 <SingleNews {...singleNews} />
               </li>
             );
@@ -63,17 +109,21 @@ const Wrapper = styled.div`
     left: -63vw;
   }
 
+  .hide {
+    display: none;
+  }
+
   .single-home {
     height: 100%;
     width: 100%;
-    ${"" /* overflow: hidden; */}
+    overflow: hidden;
     position: relative;
     margin: 1.4rem;
   }
 
   .li-item {
     width: 100%;
-    transition: 0.1s ease-in;
+    transition: 1s ease-in;
     position: absolute;
     text-transform: capitalize;
   }
